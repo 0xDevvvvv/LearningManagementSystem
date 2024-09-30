212577c851aad4514107776e3dbf7e7ff6aa86e8
Dev SK <devbhagavan25@gmail.com>
2024-09-30 23:01:33 +0530

    2nd commit

--- app/(router)/dashboard/_components/InProgressCourseLIst.jsx
+++ app/(router)/dashboard/_components/InProgressCourseLIst.jsx
@@ -4,7 +4,7 @@ export default function InProgressCourseLIst({userEnrolledCourses}) {
   return (
     <div className='p-5 bg-white mt-3 rounded-sm'>
       <h2 className='text-primary text-[18px] font-semibold'>Recent Enrolled Courses</h2>
-      <div className='grid grid-cols-1 md:grid-cols-3 mt-3'>
+      <div className='grid grid-cols-1 md:grid-cols-3 mt-3 gap-5'>
         {userEnrolledCourses?.map((item,index)=>(
           <ProgressCourseItem key={index}
           course={item}
